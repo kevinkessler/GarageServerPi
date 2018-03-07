33574c2652ff9788886ee60e8c1f9f505b80ffe0
Kevin Kessler <kkessler@kkessler.com>
2018-03-06 22:01:15 -0500

    One Humidity Sensor

--- gserv/SensorModule.py
+++ gserv/SensorModule.py
@@ -31,10 +31,7 @@ class SensorModule(BaseModule):
       self.lux_topic = self.config['lux_topic']
       self.temp_topic = self.config['temp_topic']
       self.hum_topic = self.config['hum_topic']
-      self.temp_topic2 = self.config['temp_topic2']
-      self.hum_topic2 = self.config['hum_topic2']
       self.max_addr = self.config['max_addr']
-      self.si_addr = self.config['si_addr']
       self.hih_addr = self.config['hih_addr']
       self.sub_topic = self.config['sub_topic']
     except KeyError as e:
@@ -55,7 +52,6 @@ class SensorModule(BaseModule):
     while True:
       self.post_lux()
       self.post_temp()
-      self.post_temp2()
       time.sleep(1)
 
   def post_lux(self):
@@ -67,30 +63,14 @@ class SensorModule(BaseModule):
 
   def post_temp(self):
     logger = logging.getLogger(__name__)
-    data = self.bus.read_word_data(0x40, 0xe5)
-    data_l = (data & 0xff00) >> 8
-    data_h = data & 0x00ff
-    hum = ((data_h * 256 + data_l) * 125 / 65536.0) - 6
-    logger.debug("Hum1 = {}".format(hum))
-    self.mqtt_client.publish(self.hum_topic, str(hum), retain=True)
-
-    data = self.bus.read_word_data(0x40, 0xe0)
-    data_l = (data & 0xff00) >> 8
-    data_h = data & 0x00ff
-    temp = ((data_h * 256 + data_l) * 175.72 / 65536.0) - 46.85
-    logger.debug("Temp1 = {}".format(temp))
-    self.mqtt_client.publish(self.temp_topic, str(temp), retain=True)
-
-  def post_temp2(self):
-    logger = logging.getLogger(__name__)
     data = self.bus.read_i2c_block_data(self.hih_addr, 0x00, 4)
     humidity = ((((data[0] & 0x3F) * 256) + data[1]) * 100.0) / 16383.0
     temp = (((data[2] & 0xFF) * 256) + (data[3] & 0xFC)) / 4
     cTemp = (temp / 16384.0) * 165.0 - 40.0
-    logger.debug("Hum2 = {}".format(humidity))
-    logger.debug("Temp2 = {}".format(cTemp))
-    self.mqtt_client.publish(self.temp_topic2, str(cTemp), retain=True)
-    self.mqtt_client.publish(self.hum_topic2, str(humidity), retain=True)
+    logger.debug("Hum = {}".format(humidity))
+    logger.debug("Temp = {}".format(cTemp))
+    self.mqtt_client.publish(self.temp_topic, str(cTemp), retain=True)
+    self.mqtt_client.publish(self.hum_topic, str(humidity), retain=True)
 
   def lightReading(self, data_h, data_l):
     exp = (data_h & 0xF0) >> 4
